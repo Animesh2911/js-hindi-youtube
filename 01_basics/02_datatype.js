6ad4977da79a5fd7a0331f59ba4c4627643ea166
Animesh kumar <117725106+animeshtiwari420@users.noreply.github.com>
2024-03-30 19:26:16 +0000

    "main"

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as never version
+
+//alert( 3+ 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+ // code readability should be high
+console.log("Animesh")
+
+let name = "Animesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string =>""
+//boolean => true/false
+//null => standalone value
+////undefined => 
+// symbol => unique
+//
+
+
+console.log(typeof "Animesh");
