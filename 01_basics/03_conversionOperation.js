b5c827f05e63d1490f08b8eeecbd6320c32b2b54
Animesh kumar <117725106+animeshtiwari420@users.noreply.github.com>
2024-04-02 11:41:54 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+
+
+// let score = "33abc"
+let score = null 
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//notes
+//"33"=> 33
+// "33abs"=> NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+//.log(typeof stringNumber);
+
+// *****************Operations*************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log("1" + 2);
+//console.log(2 + "1");
+ //console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log( (3 + 4) * 5 %3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
